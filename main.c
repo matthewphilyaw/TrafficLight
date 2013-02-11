5c8dc53f2a7e60f3089626e3b8365c7b4621fa70
Matthew Philyaw <matthew.philyaw@gmail.com>
2013-02-11 00:28:48 -0500

    fixed formatting isseus

--- main.c
+++ main.c
@@ -3,22 +3,21 @@
 
 int main(void)
 {
-	DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
-	PORTB = 0;
-	
-	const int mGreen = 10 * 1e3;
-	const int mYellow = 3 * 1e3;
-	const int mRed = mGreen + mYellow;
-	
+    DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
+    PORTB = 0;
+    int mGreen = 10 * 1e3;
+    const int mYellow = 3 * 1e3;
+    const int mRed = mGreen + mYellow;
+
     while(1)
     {
-		PORTB = (1 << DDB2); 
-		_delay_ms(mGreen);
-		
-		PORTB = (1 << DDB1);
-		_delay_ms(mYellow);
-		
-		PORTB = (1 << DDB0);
-		_delay_ms(mRed);
+        PORTB = (1 << DDB2); 
+        _delay_ms(mGreen);
+
+        PORTB = (1 << DDB1);
+        _delay_ms(mYellow);
+
+        PORTB = (1 << DDB0);
+        _delay_ms(mRed);
     }
 }
